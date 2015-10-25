d772e946da91d9e879e73d37c23ebf5f5ec2f99a
Nick Frost <nickfrostatx@gmail.com>
2015-10-25 16:55:11 -0700

    Add some tests

--- tests/test_error.py
+++ tests/test_error.py
@@ -5,6 +5,38 @@ from flask import Flask, abort
 import corral.error
 
 
+def test_custom_handler():
+    app = Flask(__name__)
+    client = app.test_client()
+
+    @corral.error.handle_errors(app, include_500=True)
+    def handle(e):
+        return e.name + '\n', e.code
+
+    @app.route('/good')
+    def good():
+        return 'OK'
+
+    @app.route('/404')
+    def notfound():
+        abort(404)
+
+    @app.route('/internal')
+    def divide_by_zero():
+        1/0
+
+    rv = client.get('/good')
+    assert rv.status_code == 200
+
+    rv = client.get('/404')
+    assert rv.data == b'Not Found\n'
+    assert rv.status_code == 404
+
+    rv = client.get('/internal')
+    assert rv.data == b'Internal Server Error\n'
+    assert rv.status_code == 500
+
+
 def test_html_handler():
     app = Flask(__name__, template_folder='../corral/templates')
     client = app.test_client()
@@ -12,16 +44,15 @@ def test_html_handler():
 
     @app.route('/good')
     def good():
-        return ''
-
-    @app.route('/internal')
-    def divide_by_zero():
-        arr = [1, 2]
-        return str(arr[2])
+        return 'OK'
 
     @app.route('/404')
     def notfound():
         abort(404)
+
+    @app.route('/internal')
+    def divide_by_zero():
+        1/0
 
     rv = client.get('/good')
     assert rv.status_code == 200
