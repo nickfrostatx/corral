44d76be3dfe432f591df3fef979332c34a48b3c3
Nick Frost <nickfrostatx@gmail.com>
2015-10-25 16:54:19 -0700

    Change error handling logic

--- corral/error.py
+++ corral/error.py
@@ -5,13 +5,16 @@ from flask import render_template
 from werkzeug.exceptions import HTTPException, InternalServerError
 from functools import wraps
 
-ERROR_CODES = [400, 401, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414,
+ERROR_CODES = (400, 401, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414,
                415, 416, 417, 418, 422, 423, 424, 426, 428, 429, 431, 501, 502,
-               503, 504, 505]
+               503, 504, 505)
 
 
 def handle_errors(app, include_500=False):
-    """Return a function decorator to catch all errors on app."""
+    """Return a function decorator to catch all errors on app.
+
+    :param include_500: If set to true, register a handler for 500
+    """
     def wrapper(fn):
         """Register fn as an error handler for app."""
         @wraps(fn)
@@ -19,11 +22,12 @@ def handle_errors(app, include_500=False):
             if not isinstance(e, HTTPException):
                 e = InternalServerError()
             return fn(e)
-        codes = list(ERROR_CODES)
-        if include_500:
-            codes.append(500)
-        for status in codes:
+
+        for status in ERROR_CODES:
             app.errorhandler(status)(inner_handle)
+        if include_500:
+            app.errorhandler(500)(inner_handle)
+
         return fn
     return wrapper
 
