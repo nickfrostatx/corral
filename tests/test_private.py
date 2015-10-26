ad12e61fb4366163d8115e54a6efb31248de3b29
Nick Frost <nickfrostatx@gmail.com>
2015-10-26 00:55:47 -0700

    Test crossdomain

--- tests/test_private.py
+++ tests/test_private.py
@@ -25,3 +25,30 @@ def test_auth():
         data = json.loads(rv.data.decode('utf-8'))
         assert data['msg'] == 'Not Found'
         assert rv.status_code == 404
+
+
+def test_cors(httpbin, tmpdir):
+    app = create_app()
+    app.config['SITES'] = {
+        'site1': {
+            'origin': httpbin.url,
+            'filename': '%d.jpg',
+            'path': str(tmpdir),
+            'url': httpbin.url + '/status/%d',
+        },
+    }
+    app.config['AUTH_KEY'] = 'secret'
+
+    with app.test_client() as c:
+        rv = c.post('/download/site1/123', headers={'Cookie': 'key=secret'})
+        assert 'Access-Control-Allow-Origin' not in rv.headers
+
+        rv = c.post('/download/site1/418', headers={'Origin': httpbin.url,
+                                                    'Cookie': 'key=secret'})
+        assert rv.headers.get('Access-Control-Allow-Origin') == httpbin.url
+        assert rv.status_code == 400
+
+        rv = c.post('/download/site1/418', headers={'Origin': 'http://goo.gl',
+                                                    'Cookie': 'key=secret'})
+        assert rv.headers.get('Access-Control-Allow-Origin') == httpbin.url
+        assert rv.status_code == 400
